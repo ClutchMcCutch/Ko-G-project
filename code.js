6c504524ec49af89b125fd832e3065722b00179d
chipsomen <112735090+chipsomen@users.noreply.github.com>
2022-11-11 17:07:26 -0500

    added art folder, and background changing function

--- code.js
+++ code.js
@@ -1,27 +1,45 @@
 function openCity(evt, cityName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("city");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < x.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " w3-red";
+  var i, x, tablinks;
+  x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " w3-red";
 }
 
 function openLink(evt, animName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("city");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
-    }
-    document.getElementById(animName).style.display = "block";
-    evt.currentTarget.className += " w3-red";
+  var i, x, tablinks;
+  x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+  }
+  document.getElementById(animName).style.display = "block";
+  evt.currentTarget.className += " w3-red";
+}
+
+
+var current = 0
+function changeBackground(){
+  var body, bgList;
+  bgList = [
+    "url(art/mali.jpg)",
+    "url(art/dance.jpg)",
+    "url(art/islam.jpg)",
+    "url(art/musa.jpg)"
+  ];
+  body = document.body.style;
+  current++
+  current = current % bgList.length;
+  body.backgroundImage = bgList[current]
+
+  console.log(current, bgList[current])
+}
